aeb11fe87459b2feedad40c8833c7c6b582a923b
Funda <fzelihadogan@gmail.com>
2022-12-18 14:20:51 -0500

    day7

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -21,10 +21,10 @@ public abstract class TestBase {
         driver.manage().window().maximize();
     }
 
-    @After
-    public void tearDown(){
-
-        driver.quit();
-    }
+//    @After
+//    public void tearDown(){
+//
+//        driver.quit();
+//    }
 
 }
