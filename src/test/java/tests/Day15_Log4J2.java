e7cfe7c1fe277a812b4eb67d60a6e2715f9b59f5
Funda <fzelihadogan@gmail.com>
2023-01-07 17:41:16 -0500

    Day15

--- src/test/java/tests/Day15_Log4J2.java
+++ src/test/java/tests/Day15_Log4J2.java
@@ -10,14 +10,5 @@ public class Day15_Log4J2 {
     //create object
     private static Logger logger = LogManager.getLogger(Day15_Log4J2.class.getName());
 
-    @Test
-    public void log4jTest() {
 
-        // use logger object to log/print information
-        logger.fatal("Fatal Log !!!"); // prints by default
-        logger.error("Error Log !!!"); // prints by default
-        logger.warn("Warning Log !!!");
-        logger.debug("Debug Log !!!");
-        logger.info("Info Log !!!");
-    }
 }
